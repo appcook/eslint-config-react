165b5cc9f69595e2c8ac0aaddfea822e1c8b1335
Wang Chi <hiwangchi@gmail.com>
2021-08-12 11:43:35 +0800

    Update examples :clapper:

---
+++ examples/vite-react-app/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// test rules
+const a = 1;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
